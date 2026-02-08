e34c9c3f4afdfb5bfaa686cc1ef84c1a585502de
Bennett Smrdel <fireknights318@gmail.com>
2026-02-08 08:46:56 -0600

    feat: Create render pipeline

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -20,6 +20,7 @@ pub struct State {
   config: wgpu::SurfaceConfiguration,
   is_surface_configured: bool,
   mouse_position: PhysicalPosition<f64>,
+  render_pipeline: wgpu::RenderPipeline,
   window: Arc<Window>
 }
 
@@ -73,6 +74,56 @@ impl State {
       desired_maximum_frame_latency: 2
     };
 
+    let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+      label: Some("Shader"),
+      source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into())
+    });
+
+    let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+      label: Some("Render Pipeline Layout"),
+      bind_group_layouts: &[],
+      immediate_size: 0
+    });
+
+    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+      label: Some("Render Pipeline"),
+      layout: Some(&render_pipeline_layout),
+      vertex: wgpu::VertexState {
+        module: &shader,
+        entry_point: Some("vs_main"),
+        buffers: &[],
+        compilation_options: wgpu::PipelineCompilationOptions::default()
+      },
+      fragment: Some(wgpu::FragmentState {
+        module: &shader,
+        entry_point: Some("fs_main"),
+        targets: &[Some(wgpu::ColorTargetState {
+          format: config.format,
+          blend: Some(wgpu::BlendState::REPLACE),
+          write_mask: wgpu::ColorWrites::ALL
+        })],
+        compilation_options: wgpu::PipelineCompilationOptions::default()
+      }),
+      primitive: wgpu::PrimitiveState {
+        topology: wgpu::PrimitiveTopology::TriangleList,
+        strip_index_format: None,
+        front_face: wgpu::FrontFace::Ccw,
+        cull_mode: Some(wgpu::Face::Back),
+        polygon_mode: wgpu::PolygonMode::Fill,
+        unclipped_depth: false,
+        conservative: false
+      },
+      depth_stencil: None,
+      multisample: wgpu::MultisampleState {
+        count: 1,
+        mask: !0,
+        alpha_to_coverage_enabled: false
+      },
+      multiview_mask: None,
+      cache: None
+    });
+
+
     Ok(Self {
       surface,
       device,
@@ -80,6 +131,7 @@ impl State {
       config,
       is_surface_configured: false,
       mouse_position: PhysicalPosition::new(0.0, 0.0),
+      render_pipeline,
       window
     })
   }
@@ -123,27 +175,34 @@ impl State {
     });
 
     {
-      let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+      let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
         label: Some("Render Pass"),
-        color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-          view: &view,
-          resolve_target: None,
-          depth_slice: None,
-          ops: wgpu::Operations {
-            load: wgpu::LoadOp::Clear(wgpu::Color {
-              r: 0.1,
-              g: 0.2,
-              b: 0.3,
-              a: 1.0
-            }),
-            store: wgpu::StoreOp::Store,
-          },
-        })],
+        color_attachments: &[
+          Some(wgpu::RenderPassColorAttachment {
+            view: &view,
+            depth_slice: None,
+            resolve_target: None,
+            ops: wgpu::Operations {
+              load: wgpu::LoadOp::Clear(
+                wgpu::Color {
+                  r: 0.1,
+                  g: 0.2,
+                  b: 0.7,
+                  a: 1.0
+                }
+              ),
+              store: wgpu::StoreOp::Store
+            }
+          })
+        ],
         depth_stencil_attachment: None,
         occlusion_query_set: None,
         timestamp_writes: None,
         multiview_mask: None
       });
+
+      render_pass.set_pipeline(&self.render_pipeline);
+      render_pass.draw(0..3, 0..1);
     }
 
     self.queue.submit(std::iter::once(encoder.finish()));
